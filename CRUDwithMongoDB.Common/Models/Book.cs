eb5d7f105afdf82dc19025dc84888b8ed2603f8e
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-02-24 09:50:13 +0330

    #2 Model has been Configured

---
+++ CRUDwithMongoDB.Common/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CRUDwithMongoDB.Common.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string? Id { get; set; }
+    [BsonElement("Name")]
+    public string BookName { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+    public string Author { get; set; }
+}
